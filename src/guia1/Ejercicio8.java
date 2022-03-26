de8b7ce5a2507955d2a6e3dace4cb8d61ad3566c
Valentino Di Capua <92276538+valendc15@users.noreply.github.com>
2022-03-26 16:59:00 -0300

    Update Ejercicio8.java

--- src/guia1/Ejercicio8.java
+++ src/guia1/Ejercicio8.java
@@ -1,4 +1,15 @@
 package guia1;
 
 public class Ejercicio8 {
+    public static int ejercicio_8(int poly[],int x){
+       int n=poly.length-1;
+       int result=poly[poly.length-1];
+
+       for (int i=n-1; i>=0; i--){
+           result= result*x + poly[i];
+       }
+       return result;
+    }
+
 }
+    
