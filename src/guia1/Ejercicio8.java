5210809c24b2906ac5a8f835a92a894000eb0f0b
Valentino Di Capua <92276538+valendc15@users.noreply.github.com>
2022-03-26 17:37:09 -0300

    Update Ejercicio8.java

--- src/guia1/Ejercicio8.java
+++ src/guia1/Ejercicio8.java
@@ -1,6 +1,17 @@
 package guia1;
 
 public class Ejercicio8 {
+   
+    /*La regla de Horner permite evaluar cualquier polinomio de grado n, realizando una cantidad significativamente menor de
+      adiciones y multiplicaciones que la forma tradicional.
+      La forma tradicional requiere n adiciones para un polinomio de grado n, y (n(n-1))/2 multiplicaciones
+      asi para un polinomio de grado 4: addiciones=4, multiplicaciones=6.
+      
+      Con la regla de horner este numero es menor, haciendose más clara la diferencia cuanto mayor sea el grado.
+      para el mismo polinomio de grado 4: addiciones=4, multiplicaciones=4.
+      Por ende la regla de horner es el método más óptimo para evaluar  polinomios.  
+     */
+    
     public static int ejercicio_8(int poly[],int x){
        int n=poly.length-1;
        int result=poly[poly.length-1];
