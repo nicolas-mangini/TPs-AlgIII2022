ce677cabfb26ea9af999ddfe1c30b00eb2fc4e35
nicom <nicolas.mangini@ing.austral.edu.ar>
2022-06-03 19:01:17 -0300

    solutions added

--- src/tp3/Guide7Solution.java
+++ src/tp3/Guide7Solution.java
@@ -1,26 +1,64 @@
 package tp3;
 
 import tp3.utilsTP3.MatrixVectorOperation;
+import tp3.utilsTP3.Vector;
+
 import java.util.List;
 
 public class Guide7Solution implements Guide7 {
 
     @Override
-    public int[][] exercise_1_j(int[][] A, int c) { throw new UnsupportedOperationException("TODO"); }
+    public int[][] exercise_1_j(int[][] A, int c) {
+        int[][] B = new int[A.length][A.length];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                B[i][j] = A[i][j] * c;
+            }
+        }
+        return B;
+    }
 
     @Override
     public boolean exercise_2_a(int[][] A) {
-        throw new UnsupportedOperationException("TODO");
+        boolean isSymetric = true;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                if (A[i][j] != A[j][i])
+                    isSymetric = false;
+            }
+        }
+        return isSymetric;
     }
 
     @Override
     public boolean exercise_2_b(int[][] A) {
-        throw new UnsupportedOperationException("TODO");
+        boolean isDominant = true;
+        for (int i = 0; i < A.length; i++) {
+            int fileSum = 0;
+            for (int j = 0; j < A[i].length; j++) {
+                if (i != j) {
+                    fileSum = fileSum + Math.abs(A[i][j]);
+                }
+            }
+            if (Math.abs(A[i][i]) <= fileSum)
+                isDominant = false;
+        }
+        return isDominant;
     }
 
     @Override
     public MatrixVectorOperation exercise_3_b_i(MatrixVectorOperation op) {
-        throw new UnsupportedOperationException("TODO");
+        int[] result = new int[op.getMatrixRows()];
+        for (int i = 0; i < op.getMatrixRows(); i++) {
+            for (int j = 0; j < op.getMatrixColumns(); j++) {
+                if (i >= j - 1) {
+                    result[i] = op.multiplyPositions(i, j, j);
+                }
+            }
+        }
+        Vector vResult = new Vector(result);
+        op.setResult(vResult);
+        return op;
     }
 
     @Override
