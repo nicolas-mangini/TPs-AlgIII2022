cb2362c1a5a0bf82306cb858da208cba361ffe9b
nicom <nicolas.mangini@ing.austral.edu.ar>
2022-06-04 12:47:35 -0300

    por fin funciona gram smith

--- src/tp3/Guide7Solution.java
+++ src/tp3/Guide7Solution.java
@@ -3,7 +3,11 @@ package tp3;
 import tp3.utilsTP3.MatrixVectorOperation;
 import tp3.utilsTP3.Vector;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class Guide7Solution implements Guide7 {
 
@@ -52,7 +56,7 @@ public class Guide7Solution implements Guide7 {
         for (int i = 0; i < op.getMatrixRows(); i++) {
             for (int j = 0; j < op.getMatrixColumns(); j++) {
                 if (i >= j - 1) {
-                    result[i] = op.multiplyPositions(i, j, j);
+                    result[i] += op.multiplyPositions(i, j, j);
                 }
             }
         }
@@ -61,8 +65,120 @@ public class Guide7Solution implements Guide7 {
         return op;
     }
 
+    /*@Override
+    public List<double[]> exercise_4(List<double[]> A) {
+        if (isOrthogonal(A)) {
+            return A;
+        } else {
+            ArrayList<double[]> AOrthogonal = new ArrayList<>(A.size());
+            //1
+            AOrthogonal.add(A.get(0));
+            //2
+            double[] u2 = sustractionVector(A.get(1), scalarProduct(scalarProduct(A.get(1), A.get(0)), A.get(0)));
+            AOrthogonal.add(u2);
+            //3
+            double[] u3 = sustractionVector(sustractionVector(A.get(2), scalarProduct(scalarProduct(A.get(2), A.get(0)), A.get(0))), scalarProduct(scalarProduct(A.get(2), u2), u2));
+            AOrthogonal.add(u3);
+            return AOrthogonal;
+        }
+    }
+     */
+
+
+    /*@Override
+    public List<double[]> exercise_4(List<double[]> A) {
+        ArrayList<double[]> AOrthogonal = new ArrayList<>(A.size());
+        if (isOrthogonal(A)) {
+            return A;
+        } else {
+            AOrthogonal.add(A.get(0));
+            double[] vector = new double[A.get(1).length];
+            for (int i = 1; i < A.size(); i++) {
+                for (int j = 0; j < A.size() - 1; j++) {
+                    double[] previous = scalarProduct(scalarProduct(A.get(i), AOrthogonal.get(j)), AOrthogonal.get(j));
+                    if (i >= 2) {
+                        vector = sustractionVector(vector, previous);
+                    } else {
+                        vector = previous;
+                    }
+                }
+                AOrthogonal.add(sustractionVector(A.get(i), vector));
+            }
+        }
+        return AOrthogonal;
+    }*/
+
     @Override
     public List<double[]> exercise_4(List<double[]> A) {
-        throw new UnsupportedOperationException("TODO");
+        if (isOrthogonal(A)) {
+            return A;
+        } else {
+            ArrayList<double[]> ortogonal = new ArrayList<>(A.size());
+            double[] vector = new double[A.get(1).length];
+            ortogonal.add(vectorSobreScalar(normaVector(A.get(0)), A.get(0)));
+            for (int i = 1; i < A.size(); i++) {
+                for (int j = 0; j < i; j++) {
+                    if (!Arrays.equals(vector, new double[A.get(1).length])) { //Verifico si el vector guardado no es el nulo (creado al principio)
+                        vector = sustractionVector(vector, multiplyScalarVector(dotProduct(A.get(i), ortogonal.get(j)), ortogonal.get(j)));
+                    } else {
+                        vector = multiplyScalarVector(dotProduct(A.get(i), ortogonal.get(j)), ortogonal.get(j));
+                    }
+                }
+                ortogonal.add(vectorSobreScalar(normaVector(sustractionVector(A.get(i), vector)), sustractionVector(A.get(i), vector)));
+                vector = new double[A.get(1).length];
+            }
+            return ortogonal;
+        }
+    }
+
+    public static boolean isOrthogonal(List<double[]> result) {
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = i + 1; j < result.size(); j++) {
+                double actual = dotProduct(result.get(i), result.get(j));
+                if (actual != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static double dotProduct(double[] vector1, double[] vector2) {
+        double result = 0;
+        for (int i = 0; i < vector1.length; i++) {
+            result += vector1[i] * vector2[i];
+        }
+        return result;
+    }
+
+    public double[] sustractionVector(double[] vector1, double[] vector2) {
+        double[] vector3 = new double[vector1.length];
+        for (int i = 0; i < vector1.length; i++) {
+            vector3[i] = vector1[i] - vector2[i];
+        }
+        return vector3;
+    }
+
+    public double[] multiplyScalarVector(double s, double[] vector) {
+        double[] vector2 = new double[vector.length];
+        for (int i = 0; i < vector.length; i++) {
+            vector2[i] = s * vector[i];
+        }
+        return vector2;
+    }
+
+    public double normaVector(double[] vector) {
+        double aux = 0.0;
+        for (int i = 0; i < vector.length; i++) {
+            aux += Math.pow(vector[i], 2);
+        }
+        return Math.sqrt(aux);
+    }
+
+    public double[] vectorSobreScalar(double s, double[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = vector[i] / s;
+        }
+        return vector;
     }
 }
